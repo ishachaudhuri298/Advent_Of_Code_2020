3e939827b4f1566215d204cf10d324f11d2f5d8f
Chaudhuri <isha.chaudhuri.cw@jhuapl.edu>
2024-07-16 17:32:14 -0400

    day 2 part 2 is working somewhat

--- day2/day2part2.py
+++ day2/day2part2.py
@@ -12,16 +12,23 @@ for item in f.readlines():
     number, letter, password = item.split()
     #splits each line in the input into its specific number, letter, and password
     letter = letter[0]
-    #print(letter)
     #first character in string so here it is removing the letter from the colon
     first,second = number.split("-")
-    # print(password)
-    # print(first)
-    # print(second)
     #splits each rule for the numbers into the first and second place it needs to appear in the password
-    if (letter == password[int(first)-1]) and (letter == password[int(second)-1]):
+    if (letter == password[int(first)-1]):
         goodPasswords += 1
-        #if the letter matches the letter in the correct places in the password, then it is added to goodPasswords
+        #if the letter is in the first place it needs to be, add one to the goodPasswords
+        if (letter == password[int(second)-1]):
+            goodPasswords -= 1
+            badPasswords += 1
+            #if the letter is also in the second place it needs to be, subtract one from the goodPasswords and add one to the badPasswords
+    if (letter == password[int(second)-1]):
+        goodPasswords += 1
+        #if the letter is in the second place it needs to be, add one to the goodPasswords
+        if (letter == password[int(first)-1]):
+            goodPasswords -= 1
+            badPasswords += 1
+            #if the letter is also in the first place it needs to be, subtract one from the goodPasswords and add one to the badPasswords
     else:
         badPasswords += 1
         #if the password does not follow the rules, it is added to badPasswords
