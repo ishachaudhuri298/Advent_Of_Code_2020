5a4cfc2eb3110368a73878c208a8b8b9b1745da1
Chaudhuri <isha.chaudhuri.cw@jhuapl.edu>
2024-07-16 15:12:28 -0400

    day2part1 completegit add input.txt

--- day2/day2part1redo.py
+++ day2/day2part1redo.py
@@ -9,14 +9,15 @@ f = open("input.txt")
 
 for item in f.readlines():
     #for loop that reads through each line of the input file 
-    [number, letter, password] = item.split()
+    number, letter, password = item.split()
     #splits each line in the input into its specific number, letter, and password
-    for item in number:
-        [lower,upper] = item.split("-")
-        #splits each rule for the numbers into the lower number and the upper number
+    letter = letter[0]
+    #first character in string so here it is removing the letter from the colon
+    lower,upper = number.split("-")
+    #splits each rule for the numbers into the lower number and the upper number
     totalNumberOfLetters = password.count(letter)
     #counts how many letters are in each password and assigns to a variable
-    if (lower <= totalNumberOfLetters) and ( upper >= totalNumberOfLetters):
+    if (int(lower) <= totalNumberOfLetters) and (int(upper) >= totalNumberOfLetters):
         goodPasswords += 1
         #if the total number of letters meets the rule, then the goodPasswords variable adds one for each correct password
     else:
