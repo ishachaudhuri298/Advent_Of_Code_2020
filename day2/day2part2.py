0f7700d1cc15630bf8a20966c0bbffde63a4a692
Chaudhuri <isha.chaudhuri.cw@jhuapl.edu>
2024-07-16 16:48:29 -0400

    day 2 part 2 not working

--- day2/day2part2.py
+++ day2/day2part2.py
@@ -19,11 +19,12 @@ for item in f.readlines():
     # print(first)
     # print(second)
     #splits each rule for the numbers into the first and second place it needs to appear in the password
-    #for str in password:
     if (letter == password[int(first)-1]) and (letter == password[int(second)-1]):
         goodPasswords += 1
+        #if the letter matches the letter in the correct places in the password, then it is added to goodPasswords
     else:
         badPasswords += 1
+        #if the password does not follow the rules, it is added to badPasswords
 
 print (goodPasswords)
 print (badPasswords)
