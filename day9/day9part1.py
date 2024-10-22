4ac03ffe94abcb20bfeb018d6626f697e7ba4cc6
Chaudhuri <isha.chaudhuri.cw@jhuapl.edu>
2024-10-21 21:43:06 -0400

    all old input files I didn't commit earlier

--- day9/day9part1.py
+++ day9/day9part1.py
@@ -10,29 +10,38 @@ f = open("input.txt")
 allData = []
 possibleSums = []
 dataBefore25 = []
-validNumbers = []
+invalidNumbers = []
 #initalize variables
 
 for line in f.readlines():
     allData.append(int(line))
 #append all data to a list
-    dataAfter25 = allData[25:]
+#for entry in allData:
+    #dataAfter25 = allData[25:]
     #print(dataAfter25)
-    for number in dataAfter25:
-            #for each line greater than the 25th line, add all the data before it to a list
-            dataBefore25 = dataAfter25[number-25:number]
-            #for each value in data after 25, create a new list with the 25 numbers directly before it
-            for number1 in dataAfter25:
-                for number2 in dataAfter25:
-                    if (number1 != number2):
-                        sum = number1 + number2
-                        possibleSums.append(sum)
-                        #print(possibleSums)
-                        #for each 25 numbers before a specific number, if those two add to the number, add it to a list
-            if number in (possibleSums):
-                validNumbers.append(number)
-                #if the specific number has two numbers that add up to it in the previous 25 numbers, add it to a list
-            dataBefore25 = []
-            possibleSums = []
 
-print(validNumbers)
+#for count, number in enumerate(allData):
+        #dataAfter25 = allData[24:]
+    #count creates an index for the list
+        #for each line greater than the 25th line, add all the data before it to a list
+        #dataBefore25 = dataAfter25[count-25:count]
+        #for each value in data after 25, create a new list with the 25 numbers directly before it
+for count in range(25,len(allData)-1):
+    dataBefore25 = allData[count-25:count-1]
+    number = allData[count]
+    #number accesses each number at the specific count location because count is an index
+    #only applying code below to lines 25 and beyond in orginial alldata
+    for number1 in dataBefore25:
+        for number2 in dataBefore25:
+            if (number1 != number2):
+                sum = number1 + number2
+                possibleSums.append(sum)
+                #print(possibleSums)
+                #for each 25 numbers before a specific number, if those two add to the number, add it to a list
+    if number not in (possibleSums):
+        invalidNumbers.append(number)
+        #if the specific number has two numbers that add up to it in the previous 25 numbers, add it to a list
+    dataBefore25 = []
+    possibleSums = []
+
+print(invalidNumbers)
